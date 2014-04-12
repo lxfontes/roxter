43b99e915e355964ac20807b3e5000c60437cbba
lxfontes <lxfontes@gmail.com>
2014-04-12 17:51:17 -0400

    look at that, guess ketama might be too slow !!

--- tests/ketama_test.go
+++ tests/ketama_test.go
@@ -1,7 +1,9 @@
 package tests
 
 import (
+  "fmt"
   "github.com/lxfontes/roxter"
+  "github.com/stathat/consistent"
   "testing"
 )
 
@@ -95,4 +97,29 @@ func Test_RingRebalance(t *testing.T) {
     t.Fatal("Rebalance failed: second pass", k1, k2, k3)
   }
 
+}
+
+func Benchmark_Ketama(b *testing.B) {
+
+  r := roxter.NewRing(100)
+  r.AddNode("node1", 1)
+  r.AddNode("node2", 1)
+  r.AddNode("node3", 1)
+  r.Bake()
+
+  for i := 0; i < b.N; i++ {
+    r.Hash(fmt.Sprint("hash", i))
+  }
+
+}
+
+func Benchmark_StatHat(b *testing.B) {
+  c := consistent.New()
+  c.Add("node1")
+  c.Add("node2")
+  c.Add("node3")
+
+  for n := 0; n < b.N; n++ {
+    b.Log(c.Get(fmt.Sprint("hash", n)))
+  }
 }
